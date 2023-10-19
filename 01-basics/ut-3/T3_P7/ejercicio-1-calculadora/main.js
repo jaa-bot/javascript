ddacc47c1f6d871b5534e38cb4691c2edfcd04ba
JavierJT <73221857+jaa-bot@users.noreply.github.com>
2023-10-19 10:05:48 +0200

    yupi

---
+++ 01-basics/ut-3/T3_P7/ejercicio-1-calculadora/main.js
@@ -0,0 +1,18 @@
+const screen = document.getElementById('screen');
+const buttons = document.getElementById('buttons');
+
+buttons.addEventListener('click', (e) => {
+    const key = e.target.getAttribute('data-key');
+    if (key === 'clear') {
+        screen.textContent = '';
+    } else if (key === 'equal') {
+        try {
+            screen.textContent = eval(screen.textContent);
+        } catch (error) {
+            alert('Error en la expresión matemática');
+            screen.textContent = '';
+        }
+    } else {
+        screen.textContent += key;
+    }
+});
